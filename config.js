f45a298a00e091888cb3f8add242bf7823bdd796
luckypoem <luckypoem@gmail.com>
2024-12-07 16:04:50 -0800

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 var _config = {
     blog_name       : '用于演示的博客',         // 博客名称
-    owner           : 'laizimo',           // github 用户名
-    repo            : 'zimo-article',// github 中对应项目名
-    per_page        : '10',                    // 默认一页显示几篇文章
+    owner           : 'luckypoem',           // github 用户名
+    repo            : 'vue-github-issues-blog',// github 中对应项目名
+    per_page        : '5',                    // 默认一页显示几篇文章
     // 版权起始年份
     since_year: 2015,
     // icp备案号 ICP_license: 京ICP备1234556号-1
@@ -10,6 +10,6 @@ var _config = {
     // 设置为 true 发布后将使用 unpkg cdn
     cdn: false,
     // gitalk 配置
-    client_id: '1e401efd17c830cfcfe2',
-    client_secret: '7f8e1108070d5faa43bf4823f7077f229910d419',
+    client_id: 'Ov23li09uenokJ106MSP',
+    client_secret: 'ef3b961d1bb1e6c16d956dbab1007bbde50e4c86',
 }
